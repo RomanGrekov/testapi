8c401e7aeba9c919a992f8ba39419022924b7e36
Roman Grekov <romang@tycoint.com>
2020-02-16 15:42:46 +0200

    fixed tests

--- api_server/tests/test_1.py
+++ api_server/tests/test_1.py
@@ -1,18 +1,18 @@
 import pytest
 import requests
 import subprocess
+import time
 
 server_p = None
 
 def setup_module():
-    server_p = subprocess.Popen(["pwd", "&&", "python3", "./server.py"])
-
-def teardown_module():
-    server_p.communicate()
+    server_p = subprocess.Popen(["/usr/local/bin/python3", "/tmp/api_server/server.py"])
+    time.sleep(2)
 
 def test_example():
     assert 2+3 == 5, "Should be 5"
 
 def test_response200():
     resp = requests.get("http://127.0.0.1:80")
+    print("Response data: %s" % resp.content)
     assert resp.status_code == 200, "Status code should be 200 if server is running"
