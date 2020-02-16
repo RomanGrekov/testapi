f523ab56f07525484916624b5e45ef5580917eff
Roman Grekov <romang@tycoint.com>
2020-02-16 15:11:47 +0200

    added real test

--- api_server/tests/test_1.py
+++ api_server/tests/test_1.py
@@ -1,3 +1,13 @@
+import pytest
+import requests
+import os
+
+def setup_module():
+    os.spawnl(os.P_DETACH, "pwd && python3.6 ./server.py")
+
 def test_example():
     assert 2+3 == 5, "Should be 5"
 
+def test_response200():
+    resp = requests.get("http://127.0.0.1:80")
+    assert resp.status_code == 200, "Status code should be 200 if server is running"
